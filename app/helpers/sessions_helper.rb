67220d5592c461bc11a210819edbdccbdce12848
cedrati <olivier@cedrati.fr>
2013-03-19 22:49:42 +0100

    ok

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,8 @@
 module SessionsHelper
 
 	def sign_in(user)
-		cookies.permanent[:remember_token] = user.remember_token,
-		current_user = user
+		cookies.permanent[:remember_token] = user.remember_token
+		self.current_user = user
 	end
 
 	def signed_in?
@@ -16,4 +16,14 @@ module SessionsHelper
 	def current_user
 		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
 	end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def sign_out
+    current_user = nil
+    cookies.delete(:remember_token)
+  end
+
 end
