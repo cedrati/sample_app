f7bb18267e17607d49f8a8888e007a65cdb5ac39
cedrati <olivier@cedrati.fr>
2013-04-06 17:38:03 +0200

    fix bugz

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,12 @@
 class StaticPagesController < ApplicationController
-  def home
-  	if signed_in?
-  		@micropost = current_user.microposts.build if signed_in?
-  		@feed_items = current_user.feed.paginated(page: params[:page])
-  end
 
+  def home
+    if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
+  end
+  
   def help
   end
 
